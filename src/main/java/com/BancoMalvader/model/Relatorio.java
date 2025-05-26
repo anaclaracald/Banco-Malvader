a4f16e930effe0110556a99f9d19ed242e65f7f0
anaclaracald <ana05clara24@gmail.com>
2025-05-26 09:37:07 -0300

    Ajustando construtores para lombok annotations

--- src/main/java/com/BancoMalvader/model/Relatorio.java
+++ src/main/java/com/BancoMalvader/model/Relatorio.java
@@ -2,7 +2,9 @@ package com.BancoMalvader.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Relatorio {
     @Id @GeneratedValue
     private int idRelatorio;
@@ -18,14 +22,6 @@ public class Relatorio {
     private String tipoRelatorio;
     private String conteudo;
     private int idFuncionario;
-
-    public Relatorio(int idRelatorio, LocalDateTime dataGeracao, String tipoRelatorio, String conteudo, int idFuncionario) {
-        this.idRelatorio = idRelatorio;
-        this.dataGeracao = dataGeracao;
-        this.tipoRelatorio = tipoRelatorio;
-        this.conteudo = conteudo;
-        this.idFuncionario = idFuncionario;
-    }
 
     public void gerarRelatorio() {
         // lógica
--- src/main/java/com/BancoMalvader/model/Usuario/Cliente.java
+++ src/main/java/com/BancoMalvader/model/Usuario/Cliente.java
@@ -3,25 +3,23 @@ package com.BancoMalvader.model.Usuario;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Setter @Getter
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente extends Usuario{
     @Id @GeneratedValue
     private int idCliente;
     private double scoreCredito;
     private int idUsuario;
-
-    public Cliente(int idUsuario, String nome, String cpf, String dataNascimento, String telefone, String tipoUsuario, String senhaHash, String otpAtivo, LocalDateTime otpExpiracao, int idCliente, double scoreCredito, int idUsuario1) {
-        super(idUsuario, nome, cpf, dataNascimento, telefone, tipoUsuario, senhaHash, otpAtivo, otpExpiracao);
-        this.idCliente = idCliente;
-        this.scoreCredito = scoreCredito;
-        this.idUsuario = idUsuario1;
-    }
 
     public void cadastrarCliente() {
         // Lógica para cadastrar cliente
